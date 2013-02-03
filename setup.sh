5fb0f439165d403060c3676f5858beec628cc79d
Yamamoto Yusuke <y.yamamoto88@gmail.com>
2013-02-03 16:09:38 +0900

    update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,6 +1,6 @@
-#!/usr/local/bin/zsh
+#!/usr/bin/env zsh
 
-DOT_FILES=( .zshrc .vimrc .gitconfig .gitignore .tmux.conf .dir_colors )
+DOT_FILES=( .zshrc .zshenv .zshrc.alias .zshrc.osx .vimrc .vimrc.basic .vimrc.bundle .vimrc.clors .vimrc.fuf .vimrc.statusline .gitconfig .gitignore .tmux.conf .dir_colors )
 
 for file in ${DOT_FILES[*]}
 do

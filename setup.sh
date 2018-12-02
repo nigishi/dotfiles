721e03f3f295427c5e73b1e3e74c6e2d36becb69
ymmty <y.yamamoto88@gmail.com>
2018-12-02 22:19:34 +0900

    modify how to install

--- setup.sh
+++ setup.sh
@@ -1,6 +1,16 @@
 #!/bin/bash
 
-DOT_FILES=( .vimrc .gitconfig .gitignore .tmux.conf .dir_colors )
+DOT_FILES=(
+.vimrc
+.gitconfig
+.gitignore 
+.tmux.conf 
+.dir_colors
+)
+
+if [ ! -d ~/.tmux/plugins/tpm ]; then
+  git clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm
+fi
 
 for file in ${DOT_FILES[*]}
 do

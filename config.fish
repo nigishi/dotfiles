c5feba767ae131902bec15decff0d97c652987cf
nigishi <y.yamamoto88@gmail.com>
2020-01-14 21:34:51 +0900

    update

--- config.fish
+++ config.fish
@@ -1,1 +1,4 @@
 alias b "brew"
+alias tmux-boei='tmux source-file ~/tmuxconf/tmux.boei.conf'
+alias tmux-nssol='tmux source-file ~/tmuxconf/tmux.nssol.conf'
+alias tmux-nssol-cfm='tmux source-file ~/tmuxconf/tmux.nssol-cfm.conf'

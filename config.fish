28d8bd4155899f03628a7370ffa1310f45323005
yamamotoyusuke <y.yamamoto88@gmail.com>
2022-07-21 21:45:12 +0900

    modified git

--- config.fish
+++ config.fish
@@ -2,6 +2,7 @@ alias b "brew"
 alias bu "brew upgrade"
 alias v "vim"
 alias c "code"
+alias g "git"
 alias tmux-boei='tmux source-file ~/tmuxconf/tmux.boei.conf'
 alias tmux-nssol='tmux source-file ~/tmuxconf/tmux.nssol.conf'
 alias tmux-nssol-cfm='tmux source-file ~/tmuxconf/tmux.nssol-cfm.conf'
@@ -15,3 +16,5 @@ alias tmux-fkcn='tmux source-file ~/dotfiles/tmux/fkcn.conf'
 alias tmux-kolla='tmux source-file ~/dotfiles/tmux/kolla.conf'
 alias tmux-k8smc='tmux source-file ~/dotfiles/tmux/k8smc.conf'
 set -x GOPATH $HOME/go/
+set -x PATH /opt/homebrew/bin/ $PATH
+set -x PATH $HOME/dev/flutter/bin $PATH

764763edf2b4600697575bfb9ea1ee26809d0a97
yamamotoyusuke <y.yamamoto88@gmail.com>
2026-03-06 23:52:13 +0900

    update

--- fish/.config/fish/config.fish
+++ fish/.config/fish/config.fish
@@ -3,29 +3,37 @@ alias bu "brew upgrade"
 alias v "vim"
 alias c "code"
 alias g "git"
-alias tmux-boei='tmux source-file ~/tmuxconf/tmux.boei.conf'
-alias tmux-nssol='tmux source-file ~/tmuxconf/tmux.nssol.conf'
-alias tmux-nssol-cfm='tmux source-file ~/tmuxconf/tmux.nssol-cfm.conf'
-alias tmux-ctc-qfx='tmux source-file ~/dotfiles/tmux/ctc-qfx1.conf'
-alias tmux-ml2='tmux source-file ~/dotfiles/tmux/ml2.conf'
-alias tmux-sb='tmux source-file ~/dotfiles/tmux/sb.conf'
-alias tmux-hb='tmux source-file ~/dotfiles/tmux/hb.conf'
-alias tmux-hb-vmx='tmux source-file ~/dotfiles/tmux/hb-vmx.conf'
-alias tmux-sb-qfx='tmux source-file ~/dotfiles/tmux/sb-qfx.conf'
-alias tmux-fkcn='tmux source-file ~/dotfiles/tmux/fkcn.conf'
-alias tmux-kolla='tmux source-file ~/dotfiles/tmux/kolla.conf'
-alias tmux-k8smc='tmux source-file ~/dotfiles/tmux/k8smc.conf'
-alias wr='claude-code -f ~/prompts/weekly_review_prompt.txt'
 
-abbr -a gco git checkout
 set -x GOPATH $HOME/go/
 set -x PATH /opt/homebrew/bin/ $PATH
 set -x PATH $HOME/dev/flutter/bin $PATH
 set -x PATH ~/miniforge3/bin $PATH
 
+if status is-interactive
+    # 既存のエイリアス
+    alias c="clear"
+    alias e="exit"
+
+    # Modern Unix エイリアス
+    alias ls="eza --icons --git --no-user"
+    alias ll="eza -l --icons --git --no-user"
+    alias la="eza -la --icons --git --no-user"
+    alias tree="eza --tree --icons"
+    alias cat="bat"
+    alias grep="rg"
+
+    # ツールの初期化 (これがないと動きません)
+    starship init fish | source
+    zoxide init fish | source
+    mise activate fish | source
+
+    # Zellijの外の時、実行
+    if not set -q ZELLIJ
+        exec zellij
+    end
+end
+
+abbr -a gco git checkout
+
 # Added by LM Studio CLI (lms)
 set -gx PATH $PATH /Users/yamamoto/.lmstudio/bin
-
-# Added by Windsurf
-fish_add_path /Users/yamamoto/.codeium/windsurf/bin
-fish_add_path $HOME/.local/bin
